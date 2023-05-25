4d417dcb8b9be8372f9f5d2a3088f424827c9614
Artur Dzhus <asasin.lol10@gmail.com>
2023-05-25 10:50:58 +0300

    V2

--- RatInMaze/RatInMaze.cpp
+++ RatInMaze/RatInMaze.cpp
@@ -1,38 +1,136 @@
-﻿#include <array>
-#include <fstream>
+﻿#include <fstream>
 #include <iostream>
 #include <mutex>
-#include <queue>
 #include <random>
 #include <sstream>
-
-struct Point {
-  int x;
-  int y;
-};
-
-struct QueueNode {
-  Point Point;
-  int Dist;
-};
-
-const int RowNum[] = {-1, 0, 0, 1};
-const int ColumnNum[] = {0, -1, 1, 0};
+#include <stack>
+#include <vector>
 
 std::mutex WriteMutex;
+// Структура, що представляє вершину графу
+struct Vertex {
+  // Індекси сусідніх вершин
+  std::vector<int> neighbors;
+  // Індекс вершини, яка веде до поточної вершини
+  int previous;
+};
 
-bool IsValid(int Row, int Column, int Size);
+// Функція для перетворення лабіринту в граф
+std::vector<Vertex> createGraphFromMaze(
+    const std::vector<std::vector<int>>& maze) {
+  // Розміри лабіринту
+  int numRows = maze.size();
+  int numCols = maze[0].size();
 
-int BFS(std::vector<std::vector<int>> Maze, Point Src, Point Dest,
-        int const size);
+  // Створення графу з відповідною кількістю вершин
+  std::vector<Vertex> graph(numRows * numCols);
 
-bool SolveMaze(int Size, std::vector<std::vector<int>>  Maze);
+  // Заповнення сусідів для кожної вершини
+  for (int i = 0; i < numRows; i++) {
+    for (int j = 0; j < numCols; j++) {
+      int vertexIndex = i * numCols + j;
 
-void WriteToFile(std::ofstream &File, int Data);
+      // Перевірка можливості руху вгору
+      if (i > 0 && maze[i][j] != 1) {
+        int neighborIndex = (i - 1) * numCols + j;
+        graph[vertexIndex].neighbors.push_back(neighborIndex);
+      }
 
-void CalculateRejections(std::atomic_unsigned_lock_free &NumberOfLaunches,
-                         const int Size,
-                         std::ofstream &File);
+      // Перевірка можливості руху вниз
+      if (i < numRows - 1 && maze[i][j] != 1) {
+        int neighborIndex = (i + 1) * numCols + j;
+        graph[vertexIndex].neighbors.push_back(neighborIndex);
+      }
+
+      // Перевірка можливості руху вліво
+      if (j > 0 && maze[i][j] != 1) {
+        int neighborIndex = i * numCols + (j - 1);
+        graph[vertexIndex].neighbors.push_back(neighborIndex);
+      }
+
+      // Перевірка можливості руху вправо
+      if (j < numCols - 1 && maze[i][j] != 1) {
+        int neighborIndex = i * numCols + (j + 1);
+        graph[vertexIndex].neighbors.push_back(neighborIndex);
+      }
+    }
+  }
+
+  return graph;
+}
+
+void removeRandomEdge(std::vector<Vertex>& graph, int size) {
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_int_distribution<int> dist(size, graph.size() - size);
+
+  int randomVertex = dist(gen);
+  if (!graph[randomVertex].neighbors.empty()) {
+    std::uniform_int_distribution<int> distNeighbor(
+        0, graph[randomVertex].neighbors.size() - 1);
+    int randomNeighborIndex = distNeighbor(gen);
+    int randomNeighbor = graph[randomVertex].neighbors[randomNeighborIndex];
+
+    auto& neighbors = graph[randomVertex].neighbors;
+    neighbors.erase(neighbors.begin() + randomNeighborIndex);
+
+    auto& neighborNeighbors = graph[randomNeighbor].neighbors;
+    auto it = std::find(neighborNeighbors.begin(), neighborNeighbors.end(),
+                        randomVertex);
+    if (it != neighborNeighbors.end()) {
+      neighborNeighbors.erase(it);
+    }
+  }
+}
+
+std::vector<int> mazeSolver(std::vector<Vertex>& graph, int startVertex,
+                            int exitVertex) {
+  std::vector<bool> visited(graph.size(),
+                            false);  // Масив для відвіданих вершин
+  std::vector<int> path;  // Масив для зберігання шляху
+
+  std::stack<int> stack;  // Стек для реалізації правила лівої руки
+  stack.push(startVertex);
+  visited[startVertex] = true;
+
+  while (!stack.empty()) {
+    int currentVertex = stack.top();
+
+    if (currentVertex == exitVertex) {
+      // Досягнуто вихідну вершину, формуємо шлях
+      while (currentVertex != startVertex) {
+        path.push_back(currentVertex);
+        currentVertex = graph[currentVertex].previous;
+      }
+      path.push_back(startVertex);
+      std::reverse(path.begin(),
+                   path.end());  // Інвертуємо шлях, щоб отримати від початкової
+                                 // до кінцевої вершини
+      return path;
+    }
+
+    bool foundNext = false;
+    for (int neighbor : graph[currentVertex].neighbors) {
+      if (!visited[neighbor]) {
+        stack.push(neighbor);
+        graph[neighbor].previous = currentVertex;
+        visited[neighbor] = true;
+        foundNext = true;
+        break;
+      }
+    }
+
+    if (!foundNext) {
+      stack.pop();
+    }
+  }
+
+  return path;
+}
+
+void CalculateRejections(std::atomic_unsigned_lock_free& NumberOfLaunches,
+                         const int Size, std::ofstream& File);
+void WriteToFile(std::ofstream& File, int Data);
 
 int main() {
   std::cout << "Enter size: " << std::endl;
@@ -52,8 +150,6 @@ int main() {
 
   std::ofstream File;
 
-  // If we use half of the available threads
-  // on the processor, we get the highest productivity
   const unsigned int NumberTheards = std::thread::hardware_concurrency() / 2;
 
   std::cout << "The program was launched in: " << NumberTheards << " streams"
@@ -61,12 +157,12 @@ int main() {
 
   File.open(FileName.str(), std::fstream::app);
   std::vector<std::thread> Theards(NumberTheards);
-  for (auto &t : Theards) {
+  for (auto& t : Theards) {
     t = std::thread(&CalculateRejections, std::ref(NumberOfLaunches), Size,
                     std::ref(File));
   }
   auto Start = std::chrono::high_resolution_clock::now();
-  for (auto &t : Theards) {
+  for (auto& t : Theards) {
     t.join();
   }
   auto Stop = std::chrono::high_resolution_clock::now();
@@ -75,112 +171,39 @@ int main() {
 
   std::cout << "Duration" << Duration.count() << std::endl;
 
-  File.close();
+  return 0;
 }
 
-bool IsValid(int Row, int Column, int Size) {
-  return (Row >= 0) && (Row < Size) && (Column >= 0) && (Column < Size);
-}
-
-int BFS(std::vector<std::vector<int>> Maze, Point Src, Point Dest,
-        int const size) {
-  if (!Maze[Src.x][Src.y] || !Maze[Dest.x][Dest.y]) return -1;
-
-  std::vector<std::vector<bool>> Visited;
-  Visited.resize(size);
-  for (int i = 0; i < size; i++) {
-    Visited[i].resize(size);
-  }
-  Visited[Src.x][Src.y] = true;
-
-  std::queue<QueueNode> Queue;
-
-  QueueNode s = {Src, 0};
-  Queue.push(s);
-
-  while (!Queue.empty()) {
-    QueueNode Current = Queue.front();
-    Point pt = Current.Point;
-
-    if (pt.x == Dest.x && pt.y == Dest.y ) return Current.Dist;
-
-    Queue.pop();
-
-    for (int i = 0; i < 4; i++) {
-      int Row = pt.x + RowNum[i];
-      int Column = pt.y + ColumnNum[i];
-      if (IsValid(Row, Column, size) && Maze[Row][Column] &&
-          !Visited[Row][Column]) {
-        Visited[Row][Column] = true;
-        QueueNode Adjcell = {{Row, Column}, Current.Dist + 1};
-        Queue.push(Adjcell);
-      }
-    }
-  }
-
-  return -1;
-}
-
-bool SolveMaze(int Size, std::vector<std::vector<int>> Maze) {
-  std::vector<Point> Sources;
-  std::vector<Point> Dests;
-  for (int i = 0; i < Size; i++) {
-      Sources.push_back({0, i});
-    Dests.push_back({Size - 1, i});
-  }
-
-  for (auto i : Sources) {
-    for (auto j : Dests) {
-      int Dist = BFS(Maze, i, j, Size);
-      if (Dist != -1) {
-        return true;
-      }
-    }
-  }
-
-  return false;
-};
-
-void WriteToFile(std::ofstream &File, int Data) {
+void WriteToFile(std::ofstream& File, int Data) {
   WriteMutex.lock();
   File << Data << std::endl;
   WriteMutex.unlock();
 }
 
-void CalculateRejections(std::atomic_unsigned_lock_free &NumberOfLaunches,
-                         const int Size,
-                         std::ofstream &File) {
+void CalculateRejections(std::atomic_unsigned_lock_free& NumberOfLaunches,
+                         const int Size, std::ofstream& File) {
   while (NumberOfLaunches.load() < 100) {
     int NumberAttempts = 0;
 
-    std::vector<std::vector<int>>  Maze;
-    Maze.resize(Size);
+    std::vector<std::vector<int>> Maze;
+    Maze.resize(Size + 2);
 
-    for (int i = 0; i < Size; i++) {
+    for (int i = 0; i < Size +2; i++) {
       Maze[i].resize(Size);
     }
 
-    for (size_t i = 0; i < Size; i++) {
+    for (size_t i = 0; i < Size + 2; i++) {
       for (size_t j = 0; j < Size; j++) {
-          Maze[i][j] = 1;
-        
+        Maze[i][j] = 0;
       }
     }
 
-    while (SolveMaze(Size, Maze)) {
-      std::random_device RandomDevice;
-      std::mt19937 Gen(RandomDevice());
-      std::uniform_int_distribution<> Distribution(0, Size - 1);
+    std::vector<Vertex> graph = createGraphFromMaze(Maze);
+    int startVertex = 0;
+    int exitVertex = Maze.size() * Maze[0].size() - 1;
 
-      int i = 0;
-      int j = 0;
-
-      do {
-        i = Distribution(Gen);
-        j = Distribution(Gen);
-      } while (Maze[i][j] != 1);
-
-      Maze[i][j] = 0;
+    while (!mazeSolver(graph, startVertex, exitVertex).empty()) {
+      removeRandomEdge(graph, Size);
       NumberAttempts++;
     }
 
